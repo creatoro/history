abc9964c5e058e51827886392bcdf69330a60d62
creatoro <vito@ahogysikerul.hu>
2011-09-06 12:21:12 +0200

    Fixing the module.

--- classes/history/core.php
+++ classes/history/core.php
@@ -39,19 +39,34 @@ class History_Core {
 		if ( ! $history = Session::instance()->get('url_history'))
 		{
 			// Create URL history in session
-			$history = Session::instance()->set('url_history', array($current_url));
+			$history = Session::instance()->set('url_history', array($current_url))->as_array();
+
+			// Set history
+			$history = $history['url_history'];
 		}
 
 		if (current($history) != $current_url)
 		{
 			// Add the current URL to history as it's a new URL
 			array_unshift($history, $current_url);
+
+			// Save to session
+			$history = Session::instance()->set('url_history', $history)->as_array();
+
+			// Set history
+			$history = $history['url_history'];
 		}
 
 		if (self::$size > 0)
 		{
             // Remove URLs from history that exceed the history size limit
-			$history = Session::instance()->set('url_history', array_unique(array_slice($history, 0, self::$size)));
+			$history = array_slice($history, 0, self::$size);
+
+			// Save to session
+			$history = Session::instance()->set('url_history', $history)->as_array();
+
+			// Set history
+			$history = $history['url_history'];
         }
 
 		// Return history
@@ -74,23 +89,17 @@ class History_Core {
 	 * Returns the last URL from history.
 	 *
 	 * @static
-	 * @return  bool|string
-	 * @uses    Arr::get
+	 * @return  null|string
 	 * @uses    Session::instance
+	 * @uses    Arr::get
 	 */
 	public static function last()
     {
 		// Set history
 		 $history = Session::instance()->get('url_history', array());
 
-		if ( ! $last = end($history))
-		{
-			// No last page is available
-			return FALSE;
-		}
-		
         // Return the URL
-        return $last;
+        return Arr::get($history, 1);
     }
 
     /**
